ae12b17f3de182646c256cc1af276e45be45d7b4
Nanashi <53353250+NanashiTheNameless@users.noreply.github.com>
2025-08-30 01:17:55 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  images: { unoptimized: true },
+  eslint: { ignoreDuringBuilds: true },
+  output: 'export',
 };
 
 module.exports = nextConfig;
