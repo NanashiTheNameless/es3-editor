9138471c9b6820e6b9aed2d2feab2326ff3018ed
NamelessNanashi <53353250+NanashiTheNameless@users.noreply.github.com>
2025-08-30 18:01:21 -0500

    update

--- pages/_app.js
+++ pages/_app.js
@@ -8,12 +8,12 @@ export default function App ({ Component, pageProps }) {
       <Head>
         <title>EasySave3 Editor (NamelessNanashi Fork)</title>
         <meta property='og:site_name' content='EasySave3 Editor (NamelessNanashi Fork)' />
-        <meta property='og:image' content='https://es3.namelessnanashi.dev/logo.png' />
+        <meta property='og:image' content='https://es3.namelessnanashi.dev/favicon.png' />
         <meta
           name='keywords'
           content='EasySave3 (NamelessNanashi Fork), Save file editing, Save file manipulation, Save file management, Game save editor, Save file converter, EasySave3 compatibility, Save data modification, Online save file editor, Save file backup, Save file restore, Save file extraction, Save file compression, Save file encryption, Save file decryption, Cross-platform support, EasySave3 integration, Save file analysis, Save file troubleshooting, User-friendly EasySave3 interface, Auto-save detection, Save file validation, Save file synchronization, Save file sharing, Save file recovery, Save file manipulation tools, Save file versioning'
         />
-
+        <link rel="icon" type="image/x-icon" href="/favicon.ico"></link>
         <meta name='viewport' content='initial-scale=1.0, width=device-width' />
       </Head>
         <ChakraProvider value={defaultSystem}>
